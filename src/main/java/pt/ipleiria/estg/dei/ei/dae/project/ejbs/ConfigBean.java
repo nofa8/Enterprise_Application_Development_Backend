dbe10b2a85078ff279da594ccfca171336596304
nofa8 <dom.afonsoduarte@gmail.com>
2025-01-18 22:54:53 +0000

    working

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/ConfigBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/ConfigBean.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
+import pt.ipleiria.estg.dei.ei.dae.project.dtos.SensorDataDTO;
 import pt.ipleiria.estg.dei.ei.dae.project.entities.enums.OrderState;
 import pt.ipleiria.estg.dei.ei.dae.project.entities.enums.VolumeState;
 import pt.ipleiria.estg.dei.ei.dae.project.entities.mappings.ProductVolumeMapping;
@@ -80,14 +81,14 @@ public class ConfigBean {
 
 
             // Existing Product Types
-            for (int i = 1; i <= 12; i++) {
+            for (int i = 1; i <= 5; i++) {
                 productTypeBean.create(i);
             }
 
             // Assign Sensors to Product Types
-            productTypeBean.addSensor(1, sensorTypeBean.find(1), 10);
+            productTypeBean.addSensor(1, sensorTypeBean.find(1), 2);
             productTypeBean.addSensor(2, sensorTypeBean.find(2), 5);
-            productTypeBean.addSensor(3, sensorTypeBean.find(3), 8);
+            productTypeBean.addSensor(3, sensorTypeBean.find(3), 4);
             productTypeBean.addSensor(4, sensorTypeBean.find(1), 1);
 
             // Existing Package Types
@@ -106,10 +107,18 @@ public class ConfigBean {
 
 
             // Assign Sensors to Package Types
-            packageTypeBean.addSensor(1, sensorTypeBean.find(1), 10);
-            packageTypeBean.addSensor(2, sensorTypeBean.find(2), 5);
-            packageTypeBean.addSensor(3, sensorTypeBean.find(3), 8);
-            packageTypeBean.addSensor(4, sensorTypeBean.find(1), 1);
+            packageTypeBean.addSensor(1, sensorTypeBean.find(1), 2);
+            packageTypeBean.addSensor(2, sensorTypeBean.find(2), 1);
+            packageTypeBean.addSensor(3, sensorTypeBean.find(3), 2);
+            packageTypeBean.addSensor(4, sensorTypeBean.find(4), 1);
+            packageTypeBean.addSensor(5, sensorTypeBean.find(5), 1);
+            packageTypeBean.addSensor(6, sensorTypeBean.find(2), 4);
+            packageTypeBean.addSensor(7, sensorTypeBean.find(3), 7);
+            packageTypeBean.addSensor(8, sensorTypeBean.find(5), 1);
+            packageTypeBean.addSensor(9, sensorTypeBean.find(1), 2);
+            packageTypeBean.addSensor(10, sensorTypeBean.find(5), 5);
+            packageTypeBean.addSensor(11, sensorTypeBean.find(3), 3);
+            packageTypeBean.addSensor(12, sensorTypeBean.find(1), 2);
 
             // Existing Orders
             orderBean.create(1, 1.2f, OrderState.PROCESSED, Date.from(Instant.now()), Date.from(Instant.now()), 1);
@@ -126,23 +135,64 @@ public class ConfigBean {
             orderBean.create(12, 441.5f, OrderState.SHIPPED, Date.from(Instant.now()), Date.from(Instant.now()), 1);
 
             // Existing Volumes
-            for (int i = 1; i <= 12; i++) {
+            for (int i = 1; i <= 5; i++) {
                 volumeBean.create(i, VolumeState.PACKED, i, i, Date.from(Instant.now()));
             }
 
             // Existing Sensors
             sensorBean.create(1, 1, "22.5", Date.from(Instant.now()), 1);  // Temperature Sensor - Temperature value in Celsius
-            sensorBean.create(2, 2, "1013.25", Date.from(Instant.now()), 2);  // Atmospheric Pressure Sensor - Pressure in hPa
-            sensorBean.create(3, 3, "0.05", Date.from(Instant.now()), 3);  // Accelerometer - Acceleration value in m/s²
-            sensorBean.create(4, 4, "39.908889, -8.821111", Date.from(Instant.now()), 4);  // Global Positioning Sensor - Latitude and Longitude
-            sensorBean.create(5, 5, "55", Date.from(Instant.now()), 5);  // Humidity Sensor - Humidity percentage
-            sensorBean.create(6, 1, "25.0", Date.from(Instant.now()), 6);  // Temperature Sensor - Temperature value in Celsius
-            sensorBean.create(7, 2, "1020.0", Date.from(Instant.now()), 7);  // Atmospheric Pressure Sensor - Pressure in hPa
-            sensorBean.create(8, 3, "0.10", Date.from(Instant.now()), 8);  // Accelerometer - Acceleration value in m/s²
-            sensorBean.create(9, 4, "39.900000, -8.820000", Date.from(Instant.now()), 1);  // Global Positioning Sensor - Latitude and Longitude
-            sensorBean.create(10, 5, "60", Date.from(Instant.now()), 2);  // Humidity Sensor - Humidity percentage
-            sensorBean.create(11, 1, "18.5", Date.from(Instant.now()), 3);  // Temperature Sensor - Temperature value in Celsius
-            sensorBean.create(12, 2, "1015.75", Date.from(Instant.now()), 4);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(2, 1, "25.5", Date.from(Instant.now()), 1);  // Temperature Sensor - Temperature value in Celsius
+            sensorBean.create(21, 1, "20.5", Date.from(Instant.now()), 1);  // Temperature Sensor - Temperature value in Celsius
+            sensorBean.create(22, 1, "19.5", Date.from(Instant.now()), 1);  // Temperature Sensor - Temperature value in Celsius
+
+            sensorBean.create(23, 2, "1010.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(24, 2, "1007.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(25, 2, "1002.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(26, 2, "1020.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(27, 2, "1023.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+
+            sensorBean.create(28, 2, "1010.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(29, 2, "1007.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(30, 2, "1002.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(31, 2, "1020.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(32, 2, "1023.25", Date.from(Instant.now()), 1);  // Atmospheric Pressure Sensor - Pressure in hPa
+
+
+            sensorBean.create(3, 2, "1013.25", Date.from(Instant.now()), 2);  // Atmospheric Pressure Sensor - Pressure in hPa
+
+            sensorBean.create(33, 3, "0.65", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(34, 3, "1.05", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(35, 3, "1.25", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(36, 3, "0.75", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+
+            sensorBean.create(37, 3, "1.65", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(38, 3, "1.25", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(39, 3, "1.15", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(40, 3, "1.05", Date.from(Instant.now()), 2);  // Accelerometer - Acceleration value in m/s²
+
+            sensorBean.create(4, 3, "0.05", Date.from(Instant.now()), 3);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(5, 3, "0.08", Date.from(Instant.now()), 3);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(41, 4, "39.908889,-8.821111", Date.from(Instant.now()), 3);  // Global Positioning Sensor - Latitude and Longitude
+
+
+            sensorBean.create(6, 4, "39.908889,-8.821111", Date.from(Instant.now()), 4);  // Global Positioning Sensor - Latitude and Longitude
+
+            sensorBean.create(7, 5, "55", Date.from(Instant.now()), 5);  // Humidity Sensor - Humidity percentage
+            sensorBean.create(42, 2, "1010.25", Date.from(Instant.now()), 5);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(43, 2, "1007.25", Date.from(Instant.now()), 5);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(44, 2, "1002.25", Date.from(Instant.now()), 5);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(45, 2, "1020.25", Date.from(Instant.now()), 5);  // Atmospheric Pressure Sensor - Pressure in hPa
+            sensorBean.create(46, 2, "1023.25", Date.from(Instant.now()), 5);  // Atmospheric Pressure Sensor - Pressure in hPa
+
+            sensorBean.create(47, 3, "0.65", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(48, 3, "1.05", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(49, 3, "1.25", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(50, 3, "0.75", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+
+            sensorBean.create(51, 3, "1.65", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(52, 3, "1.25", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(53, 3, "1.15", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
+            sensorBean.create(54, 3, "1.05", Date.from(Instant.now()), 5);  // Accelerometer - Acceleration value in m/s²
 
 
             // Existing Products
@@ -151,25 +201,32 @@ public class ConfigBean {
             productBean.create(113, "Terceiro", "Tesla", 3, "Elétrico", 3);
             productBean.create(114, "Quarto", "Toyota", 4, "Híbrido", 4);
             productBean.create(115, "Quinto", "Honda", 5, "SUV", 5);
-            productBean.create(116, "Sexto", "BMW", 6, "Luxo", 6);
-            productBean.create(117, "Sétimo", "Audi", 7, "Esportivo", 7);
-            productBean.create(118, "Oitavo", "Mercedes", 8, "Sedan", 8);
-            productBean.create(119, "Nono", "Chevrolet", 9, "Crossover", 9);
-            productBean.create(120, "Décimo", "Nissan", 10, "Pick-Up", 10);
+            productBean.create(116, "Sexto", "BMW", 6, "Luxo", 5);
+            productBean.create(117, "Sétimo", "Audi", 7, "Esportivo", 2);
+            productBean.create(118, "Oitavo", "Mercedes", 8, "Sedan", 3);
+            productBean.create(119, "Nono", "Chevrolet", 9, "Crossover", 4);
+            productBean.create(120, "Décimo", "Nissan", 10, "Pick-Up", 1);
 
             // Add Products to Volumes
             productVolumeMapping.create(111, 1, 1);
-            productVolumeMapping.create(112, 2, 1);
-            productVolumeMapping.create(113, 3, 1);
-            productVolumeMapping.create(114, 4, 1);
-            productVolumeMapping.create(115, 5, 1);
-            productVolumeMapping.create(116, 6, 2);
-            productVolumeMapping.create(117, 7, 2);
-            productVolumeMapping.create(118, 8, 2);
-            productVolumeMapping.create(119, 9, 2);
-            productVolumeMapping.create(120, 10, 2);
-            productVolumeMapping.create(111, 11, 3);
-            productVolumeMapping.create(112, 12, 3);
+            productVolumeMapping.create(112, 1, 2);
+
+            productVolumeMapping.create(113, 2, 1);
+
+            productVolumeMapping.create(114, 3, 1);
+            productVolumeMapping.create(115, 3, 2);
+
+            productVolumeMapping.create(116, 4, 1);
+
+            productVolumeMapping.create(117, 5, 1);
+            productVolumeMapping.create(118, 5, 2);
+
+            for (int i = 0; i < 5; i++){
+                int wow = i+15;
+                double wow2 = 1.65 + i*0.01;
+                sensorBean.createNewSingleValue(new SensorDataDTO(Integer.toString(wow),1));
+                sensorBean.createNewSingleValue(new SensorDataDTO(Double.toString(wow2),37));
+            }
 
 
         } catch (Exception e) {
